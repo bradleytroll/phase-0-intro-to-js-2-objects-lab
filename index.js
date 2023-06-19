471e492db0d79398b99999affe6ce8178790413e
Bradley Troll <bradleytroll@gmail.com>
2023-06-19 15:11:24 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,57 +1,87 @@
-const employee = { 
-    name: "Bradley",
-    streetAddress: "123 Main Street" }; 
+// Redoing old labs for extra practice. New code at top; old code commented out below.
 
-    // pass test 1 //
-
-function nondestructivelyUpdateObject(obj, key, value) {
-    const newObj = { ...obj };
-      
-    newObj[key] = value;
-      
-    return newObj;
-      }
+const employee = {
+    name: '',
+    address: '',
+};
 
 function updateEmployeeWithKeyAndValue(obj, key, value) {
-    const UpdatedEmployee = nondestructivelyUpdateObject(employee, key, value);
-    return UpdatedEmployee;
-}
-const updatedEmplyee = updateEmployeeWithKeyAndValue(employee, "Sam", "11 Broadway" );
-
-// pass test 2 //
-
-function destructivelyUpdateObject(obj, key, value) {
-    const newObj = { ...obj };
-      
-    newObj[key] = value;
-      
-    return newObj;
-      }
+    return {
+    ...obj, 
+    [key]: value,
+};
+};
 
 function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
     obj[key] = value;
-    if (key === "name") {
-        employee.street = "12 Broadway"
-    }
-    return obj
+    return obj;
 }
-const updatedEmplyee2 = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "12 Broadway" );
-
-// pass test 3 //
 
 function deleteFromEmployeeByKey(obj, key) {
     const newObj = { ...obj };
     delete newObj[key];
     return newObj;
-  }
-  
-  const updatedEmployee3 = deleteFromEmployeeByKey(employee, "Sam");
-  
-// pass test 4 //
+};
 
 function destructivelyDeleteFromEmployeeByKey(obj, key) {
     delete obj[key];
     return obj;
 }
- const updatedEmployee4 = destructivelyDeleteFromEmployeeByKey(employee["Sam"]);
+
+// const employee = { 
+//     name: "Bradley",
+//     streetAddress: "123 Main Street" }; 
+
+//     // pass test 1 //
+
+// function nondestructivelyUpdateObject(obj, key, value) {
+//     const newObj = { ...obj };
+      
+//     newObj[key] = value;
+      
+//     return newObj;
+//       }
+
+// function updateEmployeeWithKeyAndValue(obj, key, value) {
+//     const UpdatedEmployee = nondestructivelyUpdateObject(employee, key, value);
+//     return UpdatedEmployee;
+// }
+// const updatedEmplyee = updateEmployeeWithKeyAndValue(employee, "Sam", "11 Broadway" );
+
+// // pass test 2 //
+
+// function destructivelyUpdateObject(obj, key, value) {
+//     const newObj = { ...obj };
+      
+//     newObj[key] = value;
+      
+//     return newObj;
+//       }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+//     obj[key] = value;
+//     if (key === "name") {
+//         employee.street = "12 Broadway"
+//     }
+//     return obj
+// }
+// const updatedEmplyee2 = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "12 Broadway" );
+
+// // pass test 3 //
+
+// function deleteFromEmployeeByKey(obj, key) {
+//     const newObj = { ...obj };
+//     delete newObj[key];
+//     return newObj;
+//   }
+  
+//   const updatedEmployee3 = deleteFromEmployeeByKey(employee, "Sam");
+  
+// // pass test 4 //
+
+// function destructivelyDeleteFromEmployeeByKey(obj, key) {
+//     delete obj[key];
+//     return obj;
+// }
+//  const updatedEmployee4 = destructivelyDeleteFromEmployeeByKey(employee["Sam"]);
     
